53e1397f7bf3c5a5156f1222c5a643f6b1e7ce95
omkarvishnoi1 <omkarvishnoi9@gmail.com>
2025-04-18 22:01:51 +0530

    new commit

--- backend/routes/company.route.js
+++ backend/routes/company.route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
+import {singleUpload} from "../middlewares/multer.js";
 import { getCompany, getCompanyById, registerCompany, updateCompany } from "../controllers/company.controller.js";
 
 
@@ -8,6 +9,6 @@ const router = express.Router();
 router.route("/register").post(isAuthenticated,registerCompany);
 router.route("/get").get(isAuthenticated,getCompany);
 router.route("/get/:id").get(isAuthenticated,getCompanyById);
-router.route("/update/:id").put(isAuthenticated, updateCompany);
+router.route("/update/:id").put(isAuthenticated,singleUpload ,updateCompany);
 
 export default router;
