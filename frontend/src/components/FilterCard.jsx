9275f0db1ef910c0c8084ff6d7700882c3b3056f
omkarvishnoi1 <omkarvishnoi9@gmail.com>
2025-04-19 13:33:34 +0530

    completed

--- frontend/src/components/FilterCard.jsx
+++ frontend/src/components/FilterCard.jsx
@@ -15,7 +15,7 @@ const fitlerData = [
     },
     {
         fitlerType: "Salary",
-        array: ["0-40k", "42-1lakh", "1lakh to 5lakh","5lakh to 10lakh"]
+        array: ["0-40k", "40k-1lakh", "1lakh to 5lakh","5lakh to 10lakh"]
     },
 ]
 
